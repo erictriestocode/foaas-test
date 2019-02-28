03c99d789cc4eb980a88dbe76ecdb9baea9f9f83
erictriestocode <43325093+erictriestocode@users.noreply.github.com>
2019-02-28 00:30:20 -0600

    ugh done for the night

--- main.js
+++ main.js
@@ -3,12 +3,12 @@ const XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 var xhr = new XMLHttpRequest();
 
 function fuckTest() {
-    xhr.open("GET", "http://foaas.com/awesome/:eric", true);
-    xhr.onreadystatechange = function () {
-        if (xhr.readyState != 4 || xhr.status != 200) return;
-        console.log(xhr.response);
-    };
-    
+    // xhr.open("GET", "http://foaas.com/awesome/:eric", true);
+    // xhr.onreadystatechange = function () {
+    //     if (xhr.readyState != 4 || xhr.status != 200) return;
+    //     console.log(xhr.response);
+    // };
+    loadJSON("http://foaas.com/bus/eric&",)
     console.log("something worked");
 
 }
